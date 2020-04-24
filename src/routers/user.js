3435ed7697f0c7b2129fc02e5b83c874b53f3df2
Harpreet Singh <HarpreetWP@gmail.com>
2020-04-24 17:37:29 +0530

    Integrating User Authentication

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,7 +8,8 @@ router.post('/users', async (req, res) => {
 
     try{
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken(user)
+        res.status(201).send({user, token})
     }catch(e) {
         res.status(400).send(e)
     }
@@ -21,6 +22,16 @@ router.get('/users', async (req, res) => {
     }catch(e){
         res.status(500).send()
     } 
+})
+
+router.post('/users/login', async (req, res) => {
+    try{
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken(user)
+        res.send({user, token:token})
+    }catch(e){
+        res.status(400).send()
+    }
 })
 
 router.get('/users/:id', async (req, res) => {
