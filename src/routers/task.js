c588424e9cf64a02410bfa9a14c70cf0a8f74f25
Harpreet Singh <HarpreetWP@gmail.com>
2020-04-28 20:39:21 +0530

    Integrated Filter, Sorting & Pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,12 +18,36 @@ router.post('/tasks', auth, async (req,res) =>{
     }
 })
 
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
+
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
 
     try{
-        const tasks = await Task.find({owner: req.user.id})
-        req.user.populate('tasks').execPopulate()
-        res.send(tasks)
+        //const tasks = await Task.find({owner: req.user.id})
+        await req.user.populate({
+            path:'tasks',
+            match,
+            options:{
+                limit:parseInt(req.query.limit),
+                skip:parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.tasks)
+
     }catch(e){
         res.status(500).send(e)
     }
@@ -43,7 +67,6 @@ router.get('/tasks/:id', auth, async(req, res) => {
         res.status(500).send(e)
     }
 })
-
 
 router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
