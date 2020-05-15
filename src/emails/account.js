721eabe6cedbb4adb520e4b46d5c836aa096d7e8
Harpreet Singh <HarpreetWP@gmail.com>
2020-05-15 15:20:27 +0530

    Sending emails on user creation & deletion

---
+++ src/emails/account.js
@@ -0,0 +1,35 @@
+const sgMail = require('@sendgrid/mail')
+
+const sendGridAPIKey = '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'
+
+sgMail.setApiKey(sendGridAPIKey)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to:email,
+        from:'info2preet@gmail.com',
+        subject:'Thanks for joining us!',
+        text:`Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to:email,
+        from:'info2preet@gmail.com',
+        subject:'Why you cancelled?',
+        text:`It\'s sad to see you go, ${name}! Can you please let us know, why you cancelled?`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
+
+// sgMail.send({
+//     to:'info2preet@gmail.com',
+//     from:'info2preet@gmail.com',
+//     subject:'This is my first creation!',
+//     text:'I hope this one actually get to you.'
+// })
