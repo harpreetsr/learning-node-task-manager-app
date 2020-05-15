7652c4a46bccf6501f9f9ca3551529c7522fdc00
Harpreet Singh <HarpreetWP@gmail.com>
2020-05-15 12:00:32 +0530

    User avatar section

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     const message = 'Site is under maintainence, please come back soon!'
-//     res.status(503).send(message)
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -18,15 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is running on port ', port)
 })
-
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-// const main = async function(){
-//     const user = await User.findById('5ea6af115420450b98d0a5e1')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-//main()
