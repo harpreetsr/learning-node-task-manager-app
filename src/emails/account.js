29ad9080890e3f36148efa89d04377f67222e41a
Harpreet Singh <HarpreetWP@gmail.com>
2020-05-16 15:29:21 +0530

    Config & Security implementation

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,13 +1,11 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendGridAPIKey = ''
-
-sgMail.setApiKey(sendGridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to:email,
-        from:'info2preet@gmail.com',
+        from:'harpreet@codeoye.com',
         subject:'Thanks for joining us!',
         text:`Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
@@ -16,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancellationEmail = (email, name) => {
     sgMail.send({
         to:email,
-        from:'info2preet@gmail.com',
+        from:'harpreet@codeoye.com',
         subject:'Why you cancelled?',
         text:`It\'s sad to see you go, ${name}! Can you please let us know, why you cancelled?`
     })
@@ -26,10 +24,3 @@ module.exports = {
     sendWelcomeEmail,
     sendCancellationEmail
 }
-
-// sgMail.send({
-//     to:'info2preet@gmail.com',
-//     from:'info2preet@gmail.com',
-//     subject:'This is my first creation!',
-//     text:'I hope this one actually get to you.'
-// })
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
     try{
         const token = req.header('Authorization').replace('Bearer ', '')
         //console.log('User: ',token)
-        const decoded = jwt.verify(token, 'thissimynewcourse')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
         if(!user){
             throw new Error()
